17f99dd51baa5bca2d3f7540aeac7cce72f3e1e2
wangpatrick57 <wang.patrick57@gmail.com>
2020-10-31 16:17:32 -0700

    added name

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,17 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  watch: {
+    $route: {
+      immediate: true,
+      handler (to, from) {
+        document.title = 'Patrick Wang'
+      }
+    }
+  }
+}
+</script>
